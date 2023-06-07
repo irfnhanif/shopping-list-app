02e3e3f7af6769ab8e34bbca77c61ff78499d4ad
candrasaga <candra.setiawan9b@gmail.com>
2023-06-08 01:12:25 +0800

    add ShippingEditActivity.java

---
+++ app/src/main/java/com/irfhan/shoppinglistapp/ShoppingEditActivity.java
@@ -0,0 +1,14 @@
+package com.irfhan.shoppinglistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoppingEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_edit_layout);
+    }
+}
