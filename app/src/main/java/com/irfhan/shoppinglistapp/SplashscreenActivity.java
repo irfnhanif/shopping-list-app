c085320af0163e0349f60c62123d984fb1773f86
danish <danishalif16@gmail.com>
2023-06-18 10:33:25 +0700

    add splashscreen

---
+++ app/src/main/java/com/irfhan/shoppinglistapp/SplashscreenActivity.java
@@ -0,0 +1,26 @@
+package com.irfhan.shoppinglistapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class SplashscreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen_layout);
+
+        getSupportActionBar().hide();
+
+        Handler h = new Handler(Looper.getMainLooper());
+        h.postDelayed(()->{
+            Intent i = new Intent(SplashscreenActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
+        }, 300);
+    }
+}
